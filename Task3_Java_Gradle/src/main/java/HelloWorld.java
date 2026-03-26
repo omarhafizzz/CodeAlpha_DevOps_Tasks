652c43425ee08077df9ab10a755820f637c854e0
omarhafizzz <oafiz90@gmail.com>
2026-03-26 01:31:15 -0400

    Create HelloWorld.java

---
+++ Task3_Java_Gradle/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("==========================================");
+        System.out.println("Hello from CodeAlpha DevOps Task 3!");
+        System.out.println("Java Application Built with Gradle and Jenkins.");
+        System.out.println("==========================================");
+    }
+}
